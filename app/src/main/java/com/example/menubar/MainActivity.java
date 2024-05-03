8e3864ad4bd0db75481b271c88e6080f249e1e9b
kevo <kvnsriker123@gmail.com>
2024-05-03 16:14:57 -0600

    AddFragment - By Kevo

--- app/src/main/java/com/example/menubar/MainActivity.java
+++ app/src/main/java/com/example/menubar/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             int itemId = item.getItemId();
             if (itemId == R.id.home) {
                 replaceFragment(new FragmentHome());
+            } else if (itemId == R.id.add) {
+                replaceFragment(new FragmentAdd());
             }
             return true;
         });
