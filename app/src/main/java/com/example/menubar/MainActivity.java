6126e7e08abebd0631e2b7a4f6e5cdaa63ccf9e0
Vickyr13 <estefanyarami1997@gmail.com>
2024-05-03 15:53:51 -0600

    Agregar fragments

--- app/src/main/java/com/example/menubar/MainActivity.java
+++ app/src/main/java/com/example/menubar/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.menubar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.menubar.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    //binding
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new FragmentHome());
+        binding.bottomAppBar.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new FragmentHome());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
     }
 }
